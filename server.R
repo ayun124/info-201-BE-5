41504dd55466ec49d01635d12fbb5386cd7cc80d
Hoon Park <hoonpark@Hoons-MacBook-Pro.local>
2021-06-01 14:20:37 -0700

    finished things

--- server.R
+++ server.R
@@ -7,6 +7,8 @@ library(ggplot2)
 
 tuitionData <- read.csv("tuition_cost.csv")
 diversityData <- read.csv("diversity_school.csv") 
+salaryData <- read.csv("salary_potential.csv")
+
 
 stateMaps <- map_data("state")
 
@@ -16,6 +18,22 @@ tuition <- tuitionData %>%
     select(name, state, type, degree_length, in_state_tuition, out_of_state_tuition) %>% 
     mutate(state = tolower(state))
 
+
+tuitionReform <- tuition %>% 
+    select(name,state,in_state_tuition, out_of_state_tuition)
+
+
+salaryReform <- salaryData %>% 
+    select(name,state_name,early_career_pay, mid_career_pay) %>% 
+    rename(state = state_name) %>% 
+    mutate(state = tolower(state))
+
+
+
+correlationData <- left_join(tuitionReform, salaryReform, by=c(name = "name", state = "state"), na.rm = TRUE)
+
+
+    
 
 
 shinyServer(function(input, output) {
@@ -65,8 +83,6 @@ shinyServer(function(input, output) {
             coord_quickmap() +
             labs(title = "InState Tuition Cost")+
             scale_fill_continuous(limits = c(480,60000), low = "white", high = "blue")
-            
-            
     })
     
     
@@ -76,8 +92,6 @@ shinyServer(function(input, output) {
             coord_quickmap() +
             labs(title = "Out of State Tuition Cost")+
             scale_fill_continuous(limits = c(480,60000), low = "white", high = "blue")
-            
-
     })
     
     
@@ -85,10 +99,7 @@ shinyServer(function(input, output) {
 })
 
 
-tuitionMap %>% 
-    select(out_of_state_tuition) %>% 
-    arrange(out_of_state_tuition) %>% 
-    head(5)
+
 
 
  
