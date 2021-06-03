c04350e63f6da561d9846ab1e9c7bb50402af021
Hoon Park <hoonpark@Hoons-MacBook-Pro.local>
2021-06-02 21:44:02 -0700

    pretty much done

--- server.R
+++ server.R
@@ -1,51 +1,44 @@
 
+
+# Calling Libraries to bring installed functions
 library(shiny)
 library(tidyverse)
 library(maps)
 library(ggplot2)
 
 
+
+# Reading three csv files data
 tuitionData <- read.csv("tuition_cost.csv")
 diversityData <- read.csv("diversity_school.csv") 
 salaryData <- read.csv("salary_potential.csv")
 
 
+
+# Data preparation -------------------------------------------------------------
+# Creating a stateMap data that will be used when 
+# joining with other data to create a U.S. map.
 stateMaps <- map_data("state")
 
 
-#diversity <- diversityData %>% 
-    #pivot_wider(
-               # names_from = category, 
-               # values_from = enrollment, values_fn = list(enrollment = mean)) %>% 
-    #mutate(Women = Women/total_enrollment*100,
-            #`American Indian / Alaska Native` = `American Indian / Alaska Native` / total_enrollment * 100,
-          # Asian = Asian/total_enrollment * 100,
-          # Black = Black/total_enrollment * 100,
-          # Hispanic = Hispanic/total_enrollment * 100,
-          # `Native Hawaiian / Pacific Islander` =  `Native Hawaiian / Pacific Islander` / total_enrollment * 100,
-          # White = White/total_enrollment * 100,
-          # `Two Or More Races` = `Two Or More Races` / total_enrollment * 100,
-          # Unknown = Unknown/total_enrollment * 100,
-          # `Non-Resident Foreign` = `Non-Resident Foreign`/total_enrollment * 100,
-          # `Total Minority` = `Total Minority`/total_enrollment*100)
-    
-
-
+# Mutating diversityData to have a percentage of enrollment
+# for each race, not the number of enrollment.
 diversityData <- diversityData %>% 
    mutate(enrollment = enrollment/total_enrollment * 100)
 
 
-
-
+# Creating a tuition data by filter out only the data needed
+# from tuitionData data. 
 tuition <- tuitionData %>% 
     select(name, state, type, degree_length, in_state_tuition, out_of_state_tuition) %>% 
     mutate(state = tolower(state))
 
 
+# Reforming the tuition data that will be used for correlation data.
 tuitionReform <- tuition %>% 
     select(name,state,in_state_tuition, out_of_state_tuition)
 
-
+# Reforming the salary data that will be used for correlation data
 salaryReform <- salaryData %>% 
     select(name,state_name,early_career_pay, mid_career_pay) %>% 
     rename(state = state_name) %>% 
@@ -54,16 +47,29 @@ salaryReform <- salaryData %>%
 
 
 
+# Changing the name of several race in the diversityData data
+# Since it is too long. 
+rownumbers <- which(diversityData$category == "Non-Resident Foreign")
+diversityData$category[rownumbers] <- "Foreign"
+
+rownumbers1 <- which(diversityData$category == "American Indian / Alaska Native")
+diversityData$category[rownumbers1] <- "Natives"
+
+rownumbers2 <- which(diversityData$category == "Native Hawaiian / Pacific Islander")
+diversityData$category[rownumbers2] <- "Islanders"
 
 
 
 
 
+#-------------------------------------------------------------------------------
 
-
+# Start of the Shinny server 
 shinyServer(function(input, output) {
    
+# Tuition Panel Section --------------------------------------------------------
     
+    # Creating a sample data that is reactive to the widgets
     sample <- reactive({
         if(is.null(input$degree)) {
             tuitionSelected<- tuition %>% 
@@ -71,8 +77,7 @@ shinyServer(function(input, output) {
                 group_by(state) %>% 
                 summarize(in_state_tuition = mean(in_state_tuition),
                           out_of_state_tuition = mean(out_of_state_tuition))
-                
-                
+
         } else {
             tuitionSelected <- tuition %>% 
                 filter(degree_length %in% input$degree) %>% 
@@ -81,18 +86,18 @@ shinyServer(function(input, output) {
                 summarize(in_state_tuition = mean(in_state_tuition),
                           out_of_state_tuition = mean(out_of_state_tuition))
         }
-         left_join(stateMaps, tuitionSelected,by=c(region = "state"))
-        
+         left_join(stateMaps, tuitionSelected, by=c(region = "state"))
     })
     
     
-    
-    
+    # UI widget code for check box - Degree Length
     output$degreeLength <- renderUI({
         checkboxGroupInput(inputId = "degree", label = "Which Degree Length?",
                            choices = list("2 Years" = "2 Year", "4 Years" = "4 Year"),
                            selected = "2 Year") 
     })
+    
+    # UI widget code for drop down menu - Type of School
     output$typeSchool <- renderUI({
         selectInput(inputId = "type", label = "Which type of School?",
                     choices = list("Public" = "Public", 
@@ -101,28 +106,43 @@ shinyServer(function(input, output) {
                     selected = "Public")
     })
     
+    # Map code for creating a U.S. map. 
+    
+    # In-state tuition map.
     output$map <- renderPlot({
         ggplot(sample(), aes(long, lat)) +
-            geom_polygon(aes(fill= in_state_tuition, group = group), col = "black", na.value = "black") +
+            geom_polygon(aes(fill= in_state_tuition, group = group), 
+                         col = "black", na.value = "black") +
             coord_quickmap() +
             labs(title = "InState Tuition Cost")+
-            scale_fill_continuous(limits = c(480,60000), low = "white", high = "blue")
+            scale_fill_continuous(limits = c(480,60000),
+                                  low = "white", high = "blue")
     })
+    
+    #Out-of-state tuition map.
     output$secondMap <- renderPlot({
         ggplot(sample(), aes(long, lat)) +
-            geom_polygon(aes(fill= out_of_state_tuition, group = group), col = "black") +
+            geom_polygon(aes(fill= out_of_state_tuition, group = group), 
+                         col = "black") +
             coord_quickmap() +
             labs(title = "Out of State Tuition Cost")+
-            scale_fill_continuous(limits = c(480,60000), low = "white", high = "blue")
+            scale_fill_continuous(limits = c(480,60000), 
+                                  low = "white", high = "blue")
     })
     
     
-  
+ 
     
+    
+#Correlation Panel Section -------------------------------------------------------------
+    
+    # Creating a correlation Data by combining salary and tuition data that are 
+    # reformed for joining. 
     correlationData <- left_join(salaryReform, tuitionReform, 
                                  by=c(name = "name", state = "state"))
     
     
+    # Creating a reactive data that is reactive to the state chosen
     chosenData <- reactive({
         if(any(input$state == "all")){
            selectedCorrelation <-  correlationData
@@ -133,56 +153,53 @@ shinyServer(function(input, output) {
     })
     
     
-    
+    # Drawing a scatterplot that shows the general trend of tuition vs career pay.
     output$scatterplot <- renderPlot({
         ggplot(data = chosenData())+
-            geom_point(mapping=aes(x= out_of_state_tuition, y = early_career_pay, col = state), 
+            geom_point(mapping=aes(x = out_of_state_tuition, y = early_career_pay, col = state), 
                        na.rm = TRUE)+
         labs(x = "Out of State Tuition", y = "Early Career Pay", col = "State", 
              title = "Tuition versus Early Career Pay in U.S. Colleges/Universities") 
     })
     
     
-    
-    
+    # UI widget for letting users check boxes for the state they would like to see.
     output$chosenState <- renderUI({
         checkboxGroupInput(inputId = "state", label = "Which State?",
-                           choices = list("All" = "all", "Alabama" = "alabama", "Alaska" = "alaksa",
-                                          "Arizona" = "arizona", "Arkansas" = "arkansas",
-                                          "California" = "california", "Colorado" = "colorado",
-                                          "Connecticut" = "connecticut", "Delaware" = "delaware",
-                                          "Florida" = "florida", "Georgia" = "georgia",
-                                          "Hawaii" = "hawaii", "Iadho" = "idaho",
-                                          "Illinois" = "illinois", "Indiana" = "indiana",
-                                          "Iowa" = "iowa", "Kansas" = "kansas",
-                                          "Kentucky" = "kentucky", "Louisiana" = "louisiana",
-                                          "Maine"  = "maine", "Maryland"= "maryland",
-                                          "Massachusetts" = "massachusetts",
-                                          "Michigan" = "michigan", "Minnesota" = "minnesota",
-                                          "Mississippi" = "mississippi", "Missouri" = "missouri",
-                                          "Montana" = "montana", "Nebraska" = "nebraska", 
-                                          "Nevada" = "nevada", "New Hampshire" = "new-hampshire",
-                                          "New Jersey" = "new-jersey", "New Mexico" = "new-mexico",
-                                          "New York" = "new-york", "North Carolina"="north-carolina",
-                                          "North Dakota" = "north-dakota", "Ohio" = "ohio",
-                                          "Oklahoma" = "oklahoma", "Oregon" = "oregon",
-                                          "Pennsylvania" = "pennsylvania", "Rhode-island" = "rhode-island",
-                                          "South Carolina" = "south-carolina", "South Dakota" = "south-dakota", 
-                                          "Tennessee" = "tennessee", "Texas" = "texas", 
-                                          "Utah" = "utah", "Vermont" = "vermont", 
-                                          "Virginia" = "virginia", "Washington" = "washington", 
-                                          "West Virginia" = "west-virginia", "Wisconsin" = "wisconsin", 
-                                          "Wyoming" = "wyoming"), selected = "all")
+                 choices = list("All" = "all", "Alabama" = "alabama", "Alaska" = "alaksa",
+                                "Arizona" = "arizona", "Arkansas" = "arkansas",
+                                "California" = "california", "Colorado" = "colorado",
+                                "Connecticut" = "connecticut", "Delaware" = "delaware",
+                                "Florida" = "florida", "Georgia" = "georgia",
+                                "Hawaii" = "hawaii", "Iadho" = "idaho",
+                                "Illinois" = "illinois", "Indiana" = "indiana",
+                                "Iowa" = "iowa", "Kansas" = "kansas",
+                                "Kentucky" = "kentucky", "Louisiana" = "louisiana",
+                                "Maine"  = "maine", "Maryland"= "maryland",
+                                "Massachusetts" = "massachusetts",
+                                "Michigan" = "michigan", "Minnesota" = "minnesota",
+                                "Mississippi" = "mississippi", "Missouri" = "missouri",
+                                "Montana" = "montana", "Nebraska" = "nebraska", 
+                                "Nevada" = "nevada", "New Hampshire" = "new-hampshire",
+                                "New Jersey" = "new-jersey", "New Mexico" = "new-mexico",
+                                "New York" = "new-york", "North Carolina"="north-carolina",
+                                "North Dakota" = "north-dakota", "Ohio" = "ohio",
+                                "Oklahoma" = "oklahoma", "Oregon" = "oregon",
+                                "Pennsylvania" = "pennsylvania", "Rhode-island" = "rhode-island",
+                                "South Carolina" = "south-carolina", "South Dakota" = "south-dakota", 
+                                "Tennessee" = "tennessee", "Texas" = "texas", 
+                                "Utah" = "utah", "Vermont" = "vermont", 
+                                "Virginia" = "virginia", "Washington" = "washington", 
+                                "West Virginia" = "west-virginia", "Wisconsin" = "wisconsin", 
+                                "Wyoming" = "wyoming"), selected = "all")
     
     })
     
     
+#Diversity Panel ---------------------------------------------------------------------------------------
     
-    
-    
-    
-    
-    
+    # UI Widget for radio buttons.
+    # Users can choose the state to see the rate of race diversity in universities in average.
     output$diversityState <- renderUI({
         radioButtons("diversity", label = ("Choose the State"),
                  choices = list("Alabama" = "Alabama", "Alaska" = "Alaska",
@@ -190,7 +207,7 @@ shinyServer(function(input, output) {
                                 "California" = "California", "Colorado" = "Colorado",
                                 "Connecticut" = "Connecticut", "Delaware" = "Delaware",
                                 "Florida" = "Florida", "Georgia" = "Georgia",
-                                "Hawaii" = "Hawaii", "Iadho" = "Idaho",
+                                "Hawaii" = "Hawaii", "Idaho" = "Idaho",
                                 "Illinois" = "Illinois", "Indiana" = "Indiana",
                                 "Iowa" = "Iowa", "Kansas" = "Kansas",
                                 "Kentucky" = "Kentucky", "Louisiana" = "Louisiana",
@@ -205,7 +222,7 @@ shinyServer(function(input, output) {
                                 "North Dakota" = "North Dakota", "Ohio" = "Ohio",
                                 "Oklahoma" = "Oklahoma", "Oregon" = "Oregon",
                                 "Pennsylvania" = "Pennsylvania", "Rhode-island" = "Rhode Island",
-                                "South Carolina" = "South Carolina", "South Dakota" = "South dakota", 
+                                "South Carolina" = "South Carolina", "South Dakota" = "South Dakota", 
                                 "Tennessee" = "Tennessee", "Texas" = "Texas", 
                                 "Utah" = "Utah", "Vermont" = "Vermont", 
                                 "Virginia" = "Virginia", "Washington" = "Washington", 
@@ -213,8 +230,9 @@ shinyServer(function(input, output) {
                                 "Wyoming" = "Wyoming"), selected = "Washington")
     })
     
-
     
+    # Creating a reactive data that reacts to the radio button widget.
+    # Therefore, changing the plot simultaneously, making the plot interactive.
     diData <- reactive({
         diversityData %>% 
             filter(state %in% input$diversity) %>% 
@@ -223,24 +241,14 @@ shinyServer(function(input, output) {
     })
     
     
-
-
-
-
-
+    # Creating a bar graph that shows the percentage of enrollment for each race type.
     output$barPlot <- renderPlot({
         ggplot(data = diData())+
             geom_bar(mapping = aes(x= category, y = enrollment), stat = "identity")+
             labs(x = "Races", y = "Percentage of Enrollment", col = "blue", 
-                 title = "Percentage of Enrollment for each races") 
+                 title = "Diversity Rate in Colleges/Universities by state") 
     })
-
 })
-
-
-
-
-
 
 
 
--- ui.R
+++ ui.R
@@ -1,17 +1,46 @@
 
+# Calling shiny library to enable shiny app function
 library(shiny)
 
 
+# shinyUI that draws a widget and panels on the scree of the app.
 shinyUI(fluidPage(
-  tags$style(type="text/css",
-             ".recalculating {opacity: 1.0;}"),
     tabsetPanel(
+      #Introduction tab
+      tabPanel("Introduction",
+               titlePanel("Introduction"),
+               mainPanel(
+                 br(),
+                 img(src = "collegestudents.jpeg", height = 167, width = 500),
+                 br(),
+                 br(),
+                 p(strong("Hoon Yu, Henna Lakha, Albert Yun")),
+                 br(),
+                 p("Our goal with this project was to give parents/guardians the information
+                   necessary to make an informed decision about where to send their child for
+                   higher education. We sought out to answer the following main questions for
+                   our audience:"),p(strong("How do tuition costs and fees vary across the United States?
+                   How does tuition cost relate to early career salary? And, finally, how does
+                   diversity in these higher education institutions vary across the United States?")),
+                 p("Our first dataset outlines 2018-2019 tuition costs and fees for colleges and
+                   universities across the United States. Our second dataset outlines estimated
+                   early career pay by college or university. Our third dataset outlines diversity
+                   by college or university in 2014. Our first and third datasets come from the
+                   Chronicle of Higher Education and the second dataset comes from payscale.com,
+                   but all three were initially collected by the U.S. Department of Education.
+                   We accessed these datasets from kaggle.com.")
+               )),
+      #Tuition tab.
       tabPanel("Tuition Cost", 
-               titlePanel("Tuition Cost"),
+               titlePanel("Tuition Cost from 2018-2019"),
                    sidebarLayout(
                         sidebarPanel(
                           uiOutput("typeSchool"),
-                          uiOutput("degreeLength")
+                          uiOutput("degreeLength"),
+                          br(),
+                          p("The two maps attempt to compare in/out of state tuition cost by state.
+                            Users can click on the buttons on widgets in order to customize the 
+                            type of school or degree length. Then two maps changes accordingly.")
                         ),
       
                         mainPanel(
@@ -20,31 +49,39 @@ shinyUI(fluidPage(
                         )
                     )
       ),  
-
+      
+      #Correlation tab.
       tabPanel("Correlation",
           titlePanel("Correlation between tuition and career pay"),
                sidebarLayout(
                  sidebarPanel(
-              
                    uiOutput("chosenState")
                  ),
                 mainPanel(
-                  plotOutput("scatterplot")
+                  plotOutput("scatterplot"),
+                  br(),
+                  p("This graph illustrates the positive correlation between out-of-state tuition
+                    and early career pay. Users can notice the general trend throughout the
+                    graph. They can also choose the state they wish to focus on by interacting
+                    with the widget.")
                 )
-                
                )
       ),
-      
+      #Diversity tab.
       tabPanel("Diversity",
-               titlePanel("Diversity rate by race in Universities"),
+               titlePanel("Diversity rate by race in Universities in 2014"),
                sidebarLayout(
                  sidebarPanel(
                    uiOutput("diversityState")
                  ),
                  mainPanel(
-                   plotOutput("barPlot")
+                   plotOutput("barPlot"),
+                   br(),
+                   p("This bar graph demonstrates the diversity rate in 
+                     Universities/Colleges in each state.
+                     Users can specifically choose which state they would like to 
+                     focus on.")
                  )
-                 
                )
       )
     )
